fcd6d4d9d99fb4c90d45f0f139cdbc87d4c72e31
shiva <vetvenkatashiva2@gmail.com>
2026-05-30 13:49:47 +0530

    Fix white screen: add manualChunks to isolate large libraries in Vite production build

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          pdfVendor: ['@react-pdf/renderer'],
+          charts: ['chart.js', 'react-chartjs-2'],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['@react-pdf/renderer'],
+  },
 })
